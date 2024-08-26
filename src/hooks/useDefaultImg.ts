44ca7cdd81a08c060b14d27390045ca3e4c7d3a6
wain <1550642660@qq.com>
2024-08-26 10:34:01 +0800

    8.26 尝试修复require报错4.0

--- src/hooks/useDefaultImg.ts
+++ src/hooks/useDefaultImg.ts
@@ -1,8 +1,8 @@
 
 export default function () {
     const getDefaultImg = () => {
-        return require('../assets/image/defaultImg.png')
-        // return new URL('../assets/image/defaultImg.png', import.meta.url).href; //只能本地项目用
+        // return require('../assets/image/defaultImg.png')
+        return new URL('../assets/image/defaultImg.png', import.meta.url).href; //只能本地项目用
     }
     const getImgSrc = (srcValue: string) => {
         return srcValue.length !== 0
